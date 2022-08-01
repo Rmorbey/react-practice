707a7cb10f3c04e1b182c0011b9bd3d38ff4b077
Russell <russellmorbey@hotmail.co.uk>
2022-08-01 14:47:36 +0100

    Data variables turned into a singl Object to pass as props

--- course-info/src/components/Content.js
+++ course-info/src/components/Content.js
@@ -4,9 +4,9 @@ const Content = (props) => {
   console.log("Content props:", props)
   return (
     <div>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
+      <Part part={props.course.parts[0]} />
+      <Part part={props.course.parts[1]} />
+      <Part part={props.course.parts[2]} />
     </div>
   )
 }
