27dca024829d97474f86ab3f30625bb4fad2f083
Russell <russellmorbey@hotmail.co.uk>
2022-08-02 14:42:25 +0100

    Buttons working with seperate handlers.

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Header = () => {
+  return <h1>give feedback</h1>
 }
 
-export default App;
+const Title = () => {
+  return <h1> statistics</h1>
+}
+
+const Rating = (props) => {
+  return <p>{props.text} {props.value}</p>
+}
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <Header />
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+      <Title />
+      <Rating value={good} text='good'/>
+      <Rating value={neutral} text='neutral'/>
+      <Rating value={bad} text='bad'/>
+    </div>
+  )
+}
+
+export default App
