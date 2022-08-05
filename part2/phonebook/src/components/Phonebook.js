4ee8f2a9be58785a7d29f86c0f5e7b496f948aab
Russell <russellmorbey@hotmail.co.uk>
2022-08-05 14:17:55 +0100

    Added search filter input

--- part2/phonebook/src/components/Phonebook.js
+++ part2/phonebook/src/components/Phonebook.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from '../components/Header';
 import Form from '../components/Form';
 import Contacts from '../components/Contacts';
+import UserInput from './UserInput';
 
 const Phonebook = ( { persons, formOnSubmit, formNameValue, formNumberValue, formNameOnChange, formNumberOnChange, }) => {
   return (
     <div>
       <Header title='Phonebook' />
+      <UserInput text='filter shown with' />
       <Header title='Add a new contact' />
       <Form 
       onSubmit={formOnSubmit} 
